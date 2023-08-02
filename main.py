0c33ee504bd3b428955b0b5bba58d91a4925e68b
noah560 <64210682+noah560@users.noreply.github.com>
2023-08-02 15:06:38 -0500

    Fix issue with auto-closing

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ def run():
     com = request.args.get("c")
     if easygui.boolbox("Does this website have permission to run: " + com, "permission", ["Yes", "No"]):
         bash(com.split(" "))
-    return "<script>window.close();</script>"
+    return "<script>window.setTimeout(function(){window.close()}, 100);</script>"
 
 
 app.run(host='0.0.0.0', port=9219)
